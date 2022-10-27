7346d90d192a2282aebf0e193dac090b0f253875
Hegeldirkk <hegeldirkk@gmail.com>
2022-10-27 14:01:48 +0000

    script git writting  - g\

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -2,14 +2,15 @@
 """
 starts a Flask web application
 """
-from flask import Flask
 
+from flask import Flask
 app = Flask(__name__)
+
 
 @app.route('/', strict_slashes=False)
 def hello():
     """/: display “Hello HBNB!”"""
-    return f'Hello HBNB!'
+    return 'Hello HBNB!'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
